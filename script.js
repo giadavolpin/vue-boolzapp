ff1b1a34c172ffe39538da01debf556b3927e8a6
giadavolpin <volpingiada@gmail.com>
2022-11-04 15:48:21 +0100

    html

--- script.js
+++ script.js
@@ -25,60 +25,60 @@ createApp({
                 },
             ],
             },
-        {
-        id:2,
-        name: 'Fabio',
-        avatar: '_2'
-        visible: true,
-        message: [{
-            date: '20/03/2020 16:30:00',
-            message: 'Ciao come stai?',
-            status: 'sent'
-        },
-        {
-            date: '20/03/2020 16:30:55',
-            message: 'Bene grazie! Stasera ci vediamo?',
-            status: 'received'
-        }
-    ],
-        },
-        {
-            id:3,
-            name: 'Samuele',
-            avatar: '_3',
-            visible: true,
-            messages: [{
-                date: '28/03/2020 10:10:40',
-                message: 'La Marianna va in campagna',
-                status: 'received'
-            },
-        {
-            date: '28/03/2020 10:20:10',
-            message: 'Sicuro di non aver sbagliato chat?',
-            status: 'sent'
-        },
-        {
-            date: '28/03/2020 16:15:22',
-            message: 'Ah scusa!',
-            status: 'received'
-        }],
-        },
-        {
-            id:4,
-            name: 'Luisa',
-            avatar: '_4',
-            visible: true,
-            messages: [{
-                date: '10/01/2020 15:30:55',
-                message: 'Lo sai che ha aperto una nuova pizzeria?',
-                status: 'sent'
-            },
-        {
-            date: '10/01/2020 15:30:55',
-            message:'Si, ma preferirei andare al cinema',
-            status: 'received'
-        }],
-        },
+                {
+                id:2,
+                name: 'Fabio',
+                avatar: '_2',
+                visible: true,
+                message: [{
+                    date: '20/03/2020 16:30:00',
+                    message: 'Ciao come stai?',
+                    status: 'sent'
+                },
+                {
+                    date: '20/03/2020 16:30:55',
+                    message: 'Bene grazie! Stasera ci vediamo?',
+                    status: 'received'
+                }
+            ],
+                },
+                {
+                    id:3,
+                    name: 'Samuele',
+                    avatar: '_3',
+                    visible: true,
+                    messages: [{
+                        date: '28/03/2020 10:10:40',
+                        message: 'La Marianna va in campagna',
+                        status: 'received'
+                    },
+                {
+                    date: '28/03/2020 10:20:10',
+                    message: 'Sicuro di non aver sbagliato chat?',
+                    status: 'sent'
+                },
+                {
+                    date: '28/03/2020 16:15:22',
+                    message: 'Ah scusa!',
+                    status: 'received'
+                }],
+                },
+                {
+                    id:4,
+                    name: 'Luisa',
+                    avatar: '_4',
+                    visible: true,
+                    messages: [{
+                        date: '10/01/2020 15:30:55',
+                        message: 'Lo sai che ha aperto una nuova pizzeria?',
+                        status: 'sent'
+                    },
+                {
+                    date: '10/01/2020 15:30:55',
+                    message:'Si, ma preferirei andare al cinema',
+                    status: 'received'
+                }],
+                },
         ]
         }
     },
