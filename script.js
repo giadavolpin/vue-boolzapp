862e81320663d067ec0c88bb3657a9dd6a8121e0
giadavolpin <volpingiada@gmail.com>
2022-11-04 17:28:24 +0100

    status

--- script.js
+++ script.js
@@ -3,84 +3,171 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
-            contacts: [{
-                id:1,
-                name: 'Michele',
-                avatar: '_1',
-                visible: true,
-                messages: [{
-                    date: '10/01/2020 15:30:55',
-                    message: 'Hai portato a spasso il cane?',
-                    status: 'sent'
+            currentChat : 0,
+            contacts: [
+                {
+                    name: 'Michele',
+                    avatar: '_1',
+                    visible: true,
+                    messages: [
+                        {
+                        date: '10/01/2020 15:30:55',
+                        message: 'Hai portato a spasso il cane?',
+                        status: 'sent'
+                        },
+                        {
+                        date: '10/01/2020 15:50:00',
+                        message: 'Ricordati di stendere i panni',
+                        status: 'sent'
+                        },
+                        {
+                        date: '10/01/2020 16:15:22',
+                        message: 'Tutto fatto!',
+                        status: 'received'
+                        }
+                    ],
                 },
                 {
-                    date: '10/01/2020 15:50:00',
-                    message: 'Ricordati di dargli da mangiare',
-                    status: 'sent'
-                },
-                {
-                    date: '10/01/2020 16:15:22',
-                    message: 'Tutto fatto!',
-                    status: 'received'
-                },
-            ],
-            },
-                {
-                id:2,
                 name: 'Fabio',
                 avatar: '_2',
                 visible: true,
-                message: [{
-                    date: '20/03/2020 16:30:00',
-                    message: 'Ciao come stai?',
-                    status: 'sent'
+                messages: [
+                {
+                date: '20/03/2020 16:30:00',
+                message: 'Ciao come stai?',
+                status: 'sent'
                 },
                 {
-                    date: '20/03/2020 16:30:55',
-                    message: 'Bene grazie! Stasera ci vediamo?',
-                    status: 'received'
+                date: '20/03/2020 16:30:55',
+                message: 'Bene grazie! Stasera ci vediamo?',
+                status: 'received'
+                },
+                {
+                date: '20/03/2020 16:35:00',
+                message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                status: 'sent'
                 }
-            ],
+                ],
                 },
                 {
-                    id:3,
-                    name: 'Samuele',
-                    avatar: '_3',
-                    visible: true,
-                    messages: [{
-                        date: '28/03/2020 10:10:40',
-                        message: 'La Marianna va in campagna',
-                        status: 'received'
-                    },
+                name: 'Samuele',
+                avatar: '_3',
+                visible: true,
+                messages: [
                 {
-                    date: '28/03/2020 10:20:10',
-                    message: 'Sicuro di non aver sbagliato chat?',
-                    status: 'sent'
+                date: '28/03/2020 10:10:40',
+                message: 'La Marianna va in campagna',
+                status: 'received'
                 },
                 {
-                    date: '28/03/2020 16:15:22',
-                    message: 'Ah scusa!',
-                    status: 'received'
-                }],
+                date: '28/03/2020 10:20:10',
+                message: 'Sicuro di non aver sbagliato chat?',
+                status: 'sent'
                 },
                 {
-                    id:4,
-                    name: 'Luisa',
-                    avatar: '_4',
-                    visible: true,
-                    messages: [{
-                        date: '10/01/2020 15:30:55',
-                        message: 'Lo sai che ha aperto una nuova pizzeria?',
-                        status: 'sent'
-                    },
-                {
-                    date: '10/01/2020 15:30:55',
-                    message:'Si, ma preferirei andare al cinema',
-                    status: 'received'
-                }],
+                date: '28/03/2020 16:15:22',
+                message: 'Ah scusa!',
+                status: 'received'
+                }
+                ],
                 },
-        ]
-        }
+                {
+                name: 'Alessandro B.',
+                avatar: '_4',
+                visible: true,
+                messages: [
+                {
+                date: '10/01/2020 15:30:55',
+                message: 'Lo sai che ha aperto una nuova pizzeria?',
+                status: 'sent'
+                },
+                {
+                date: '10/01/2020 15:50:00',
+                message: 'Si, ma preferirei andare al cinema',
+                status: 'received'
+                }
+                ],
+                },
+                {
+                name: 'Alessandro L.',
+                avatar: '_5',
+                visible: true,
+                messages: [
+                {
+                date: '10/01/2020 15:30:55',
+                message: 'Ricordati di chiamare la nonna',
+                status: 'sent'
+                },
+                {
+                date: '10/01/2020 15:50:00',
+                message: 'Va bene, stasera la sento',
+                status: 'received'
+                }
+                ],
+                },
+                {
+                name: 'Claudia',
+                avatar: '_6',
+                visible: true,
+                messages: [
+                {
+                date: '10/01/2020 15:30:55',
+                message: 'Ciao Claudia, hai novità?',
+                status: 'sent'
+                },
+                {
+                date: '10/01/2020 15:50:00',
+                message: 'Non ancora',
+                status: 'received'
+                },
+                {
+                date: '10/01/2020 15:51:00',
+                message: 'Nessuna nuova, buona nuova',
+                status: 'sent'
+                }
+                ],
+                },
+                {
+                name: 'Federico',
+                avatar: '_7',
+                visible: true,
+                messages: [
+                {
+                date: '10/01/2020 15:30:55',
+                message: 'Fai gli auguri a Martina che è il suo compleanno!',
+                status: 'sent'
+                },
+                {
+                date: '10/01/2020 15:50:00',
+                message: 'Grazie per avermelo ricordato, le scrivo subito!',
+                status: 'received'
+                }
+                ],
+                },
+                {
+                name: 'Davide',
+                avatar: '_8',
+                visible: true,
+                messages: [
+                {
+                date: '10/01/2020 15:30:55',
+                message: 'Ciao, andiamo a mangiare la pizza stasera?',
+                status: 'received'
+                },
+                {
+                date: '10/01/2020 15:50:00',
+                message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                status: 'sent'
+                },
+                {
+                date: '10/01/2020 15:51:00',
+                message: 'OK!!',
+                status: 'received'
+                }
+                ],
+                }
+                ]
+            }
     },
     computed: {
 
@@ -92,3 +179,21 @@ createApp({
 
     }
 },).mount('#app')
+/* 
+let array = ['obj1', 'obj2','obj3']
+for(let i = 0; i<array.length; i++){
+    console.log(array[i]);
+}
+for(element of array){
+    console.log(element)
+} */
+
+// let array = [{name:'leo'},{name:'giada'},{name:'boolean'}];
+// for(let i = 0; i<array.length; i++){
+//     console.log(array[i].name);
+// }
+// for(element of array){
+//     console.log(element.name)
+// }
+
+// array[array.length -1]  //questo mi prende l'ultimo elemento dell'array xk length mi prende la lunghezza e -1 mi permette di prendere l'indice dell'ultimo elemento.
